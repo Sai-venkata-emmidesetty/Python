c051a28d56f66c4f759334ac7703fef600d419b4
Sai-venkata-emmidesetty <141021513+Sai-venkata-emmidesetty@users.noreply.github.com>
2024-02-10 22:19:01 +0530

    Add files via upload

---
+++ Password_Generator/Password_Generator_Easy_level.py
@@ -0,0 +1,30 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+print("your password is:",end='')
+letters_select=random.sample(letters,nr_letters)
+for x in letters_select:
+    print(x, end='')
+
+
+numbers_select=random.sample(numbers,nr_numbers)
+for x in numbers_select:
+    print(x, end='')
+
+
+symbols_select=random.sample(symbols,nr_symbols)
+for x in symbols_select:
+    print(x, end='')
+
+
+
+
+
+
---
+++ Password_Generator/Readme.txt
@@ -0,0 +1,14 @@
+In this Program we will generate a random password for the user.
+
+We ask the User three questions:
+----------------------------------
+
+1)How many letters would you like in your password?
+
+2)How many symbols would you like?
+
+3)How many numbers would you like?
+
+->By taking the above 3 inputs inputs we will generate a password.
+
+->I have written 2 types of solutions for this program. Pls feel free to check both the solutions.
